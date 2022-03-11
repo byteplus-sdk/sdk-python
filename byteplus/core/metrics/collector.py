f126ff2da9ff5821a55106467e14f9a75e9f7071
xikangfang <fangxikang@bytedance.com>
2022-03-11 16:01:40 +0800

    support metrics

--- byteplus/core/metrics/collector.py
+++ byteplus/core/metrics/collector.py
@@ -2,112 +2,259 @@ import threading
 
 from metrics_option import *
 from constant import *
-from helper import *
+import helper
+from sample import *
+from metrics_pb2 import *
+import requests
+import time
+import logging
+import sys
+from byteplus.core.exception import NetException, BizException
 
+log = logging.getLogger(__name__)
+
+initialed = False  # init func should exec once
 metrics_cfg = MetricsCfg()
 metrics_collector: map = {}
+metrics_locks: map = {}
+timer_stat_metrics = ["max", "min", "avg", "pct75", "pct90", "pct95", "pct99", "pct999"]
 
 
 class MetricValue(object):
-    def __init__(self, value: float, flushed_value=None):
+    def __init__(self, value: object, flushed_value=None):
         self.value = value
         self.flushed_value = flushed_value
+        self.updated = False
+        self.lock = threading.Lock()
 
 
-class Collector(object):
-    initialed: bool = False  # init func should exec once
+# As long as the init function is called, the metrics are enabled
+def init(metrics_opts: tuple):
+    for opt in metrics_opts:
+        opt.fill(metrics_cfg)
 
-    @staticmethod
-    def init(metrics_opts: tuple):
-        for opt in metrics_opts:
-            opt.fill(metrics_cfg)
+    metrics_collector[MetricsType.metrics_type_store] = {}
+    metrics_collector[MetricsType.metrics_type_counter] = {}
+    metrics_collector[MetricsType.metrics_type_timer] = {}
 
-        metrics_collector[MetricsType.metrics_type_store] = {}
-        metrics_collector[MetricsType.metrics_type_counter] = {}
-        metrics_collector[MetricsType.metrics_type_timer] = {}
+    metrics_locks[MetricsType.metrics_type_store] = threading.Lock()
+    metrics_locks[MetricsType.metrics_type_counter] = threading.Lock()
+    metrics_locks[MetricsType.metrics_type_timer] = threading.Lock()
 
-        ## todo：初始化
-        http_cli = None
+    global initialed
+    if not initialed:
+        initialed = True
+        threading.Thread(target=report()).start()
 
-        if not Collector.initialed:
-            Collector.initialed = True
-            threading.Thread(target=Collector.report()).start()
 
-    @staticmethod
-    def report():
-        if not Collector.is_enable_metrics():
-            return
-        Collector.flushTimer()
-        Collector.flushCounter()
-        Collector.flushStore()
-
-        # a timer only execute once after spec duration
-        threading.Timer(DEFAULT_FLUSH_INTERVAL_MS / 1000, Collector.report).start()
+def report():
+    if not is_enable_metrics():
         return
+    flushTimer()
+    flushCounter()
+    flushStore()
 
-    @staticmethod
-    def flushStore():
-        pass
+    # a timer only execute once after spec duration
+    threading.Timer(metrics_cfg.flush_interval_ms / 1000, report).start()
+    return
 
-    @staticmethod
-    def flushCounter():
-        pass
 
-    @staticmethod
-    def flushTimer():
-        pass
+def flushStore():
+    metrics_requests = []
+    with metrics_locks.get(MetricsType.metrics_type_store):
+        for collect_key, metric in metrics_collector.get(MetricsType.metrics_type_store).items():
+            if metric.updated:
+                metric.updated = False
+                name, tag_kvs, ok = helper.parse_name_and_tags(collect_key)
+                if not ok:
+                    continue
+                metrics_request: Metric = Metric()
+                metrics_request.metric = metrics_cfg.prefix + "." + name
+                metrics_request.tags.update(tag_kvs)
+                metrics_request.value = metric.value
+                metrics_request.timestamp = int(time.time())
+                metrics_requests.append(metrics_request)
+    if len(metrics_requests) > 0:
+        url = OTHER_URL_FORMAT.replace("{}", metrics_cfg.domain)
+        metric_message: MetricMessage = MetricMessage()
+        metric_message.metrics.extend(metrics_requests)
+        try:
+            send(metric_message, url)
+            if enable_print_log():
+                # print("[Metrics] exec store success, url:{}, metrics_requests:{}".format(url, metrics_requests))
+                log.debug("[Metrics] exec store success, url:{}, metrics_requests:{}".format(url, metrics_requests))
+        except BaseException as e:
+            if enable_print_log():
+                print("[Metrics] exec store exception, msg:{}, url:{}, metricsRequests:{}".format(str(e), url,
+                                                                                                  metrics_requests))
+                log.error("[Metrics] exec store exception, msg:{}, url:{}, metricsRequests:{}".format(str(e), url,
+                                                                                                      metrics_requests))
 
-    @staticmethod
-    def is_enable_metrics() -> bool:
-        if metrics_cfg is None:
-            return False
-        return metrics_cfg.enable_metrics
 
-    @staticmethod
-    def enable_print_log() -> bool:
-        if metrics_cfg is None:
-            return False
-        return metrics_cfg.print_log
+def flushCounter():
+    metrics_requests = []
+    with metrics_locks.get(MetricsType.metrics_type_counter):
+        for collect_key, metric in metrics_collector.get(MetricsType.metrics_type_counter).items():
+            if metric.updated:
+                metric.updated = False
+                name, tag_kvs, ok = helper.parse_name_and_tags(collect_key)
+                if not ok:
+                    continue
+                # metric.value may not be the latest, the case is acceptable
+                value_copy = metric.value
+                metrics_request: Metric = Metric()
+                metrics_request.metric = metrics_cfg.prefix + "." + name
+                metrics_request.tags.update(tag_kvs)
+                metrics_request.value = value_copy - metric.flushed_value
+                metrics_request.timestamp = int(time.time())
+                metrics_requests.append(metrics_request)
+                metric.flushed_value = value_copy
+                # if the value is too large, reset it, it rarely happen, no lock is acceptable
+                if value_copy >= sys.float_info.max / 2:
+                    metric.value = 0.0
+                    metric.flushed_value = 0.0
 
-    @staticmethod
-    def emit_store(name: str, value: float, tag_kvs: list):
-        if not Collector.is_enable_metrics():
-            return
-        collect_key = build_collect_key(name, tag_kvs)
-        Collector.update_metric(MetricsType.metrics_type_store, collect_key, value)
+    if len(metrics_requests) > 0:
+        url = COUNTER_URL_FORMAT.replace("{}", metrics_cfg.domain)
+        metric_message: MetricMessage = MetricMessage()
+        metric_message.metrics.extend(metrics_requests)
+        try:
+            send(metric_message, url)
+            if enable_print_log():
+                log.debug("[Metrics] exec counter success, url:{}, metrics_requests:{}".format(url, metrics_requests))
+        except BaseException as e:
+            if enable_print_log():
+                log.error("[Metrics] exec counter exception, msg:{}, url:{}, metricsRequests:{}".format(str(e), url,
+                                                                                                        metrics_requests))
 
-    @staticmethod
-    def emit_counter(name: str, value: float, tag_kvs: list):
-        if not Collector.is_enable_metrics():
-            return
-        collect_key = build_collect_key(name, tag_kvs)
-        Collector.update_metric(MetricsType.metrics_type_counter, collect_key, value)
 
-    @staticmethod
-    def emit_timer(name: str, value: float, tag_kvs: list):
-        if not Collector.is_enable_metrics():
-            return
-        collect_key = build_collect_key(name, tag_kvs)
-        Collector.update_metric(MetricsType.metrics_type_timer, collect_key, value)
+def flushTimer():
+    metrics_requests = []
+    with metrics_locks.get(MetricsType.metrics_type_timer):
+        for collect_key, metric in metrics_collector.get(MetricsType.metrics_type_timer).items():
+            if metric.updated:
+                metric.updated = False
+                name, tag_kvs, ok = helper.parse_name_and_tags(collect_key)
+                if not ok:
+                    continue
+                snapshot = metric.value.snapshot()
+                metric.value.clear()
+                metrics_requests.extend(build_stat_metrics(snapshot, name, tag_kvs))
 
-    @staticmethod
-    def update_metric(metrics_type: MetricsType, collect_key: str, value: float):
-        pass
+    if len(metrics_requests) > 0:
+        url = OTHER_URL_FORMAT.replace("{}", metrics_cfg.domain)
+        metric_message: MetricMessage = MetricMessage()
+        metric_message.metrics.extend(metrics_requests)
+        try:
+            send(metric_message, url)
+            if enable_print_log():
+                log.debug("[Metrics] exec timer success, url:{}, metrics_requests:{}".format(url, metrics_requests))
+        except BaseException as e:
+            if enable_print_log():
+                log.error("[Metrics] exec timer exception, msg:{}, url:{}, metricsRequests:{}".format(str(e), url,
+                                                                                                      metrics_requests))
 
-    @staticmethod
-    def get_or_createMetric(metrics_type: MetricsType, collect_key: str):
-        if metrics_collector.get(metrics_type).get(collect_key) is not None:
-            return metrics_collector.get(metrics_type).get(collect_key)
-        threading.Lock.acquire()
+
+def build_stat_metrics(sample: SampleSnapshot, name: str, tag_kvs: map):
+    timestamp = int(time.time())
+    metrics_requests = []
+    for stat_name in timer_stat_metrics:
+        stat_func = sample.get_func_from_name(stat_name)
+        if stat_func is None:
+            continue
+        metrics_request: Metric = Metric()
+        metrics_request.metric = metrics_cfg.prefix + "." + name + "." + stat_name
+        metrics_request.tags.update(tag_kvs)
+        metrics_request.value = stat_func()
+        metrics_request.timestamp = timestamp
+        metrics_requests.append(metrics_request)
+    return metrics_requests
+
+
+def is_enable_metrics() -> bool:
+    if metrics_cfg is None:
+        return False
+    return metrics_cfg.enable_metrics
+
+
+def enable_print_log() -> bool:
+    if metrics_cfg is None:
+        return False
+    return metrics_cfg.print_log
+
+
+def emit_store(name: str, value: float, tag_kvs: list):
+    if not is_enable_metrics():
+        return
+    collect_key = helper.build_collect_key(name, tag_kvs)
+    update_metric(MetricsType.metrics_type_store, collect_key, value)
+
+
+def emit_counter(name: str, value: float, tag_kvs: list):
+    if not is_enable_metrics():
+        return
+    collect_key = helper.build_collect_key(name, tag_kvs)
+    update_metric(MetricsType.metrics_type_counter, collect_key, value)
+
+
+def emit_timer(name: str, value: float, tag_kvs: list):
+    if not is_enable_metrics():
+        return
+    collect_key = helper.build_collect_key(name, tag_kvs)
+    update_metric(MetricsType.metrics_type_timer, collect_key, value)
+
+
+def update_metric(metrics_type: MetricsType, collect_key: str, value: float):
+    metric: MetricValue = get_or_create_metric(metrics_type, collect_key)
+    if metrics_type == MetricsType.metrics_type_store:
+        metric.value = value
+    if metrics_type == MetricsType.metrics_type_counter:
+        with metric.lock:
+            metric.value = metric.value + value
+    if metrics_type == MetricsType.metrics_type_timer:
+        metric.value.update(value)
+    metric.updated = True
+
+
+def get_or_create_metric(metrics_type: MetricsType, collect_key: str):
+    if metrics_collector.get(metrics_type).get(collect_key) is not None:
+        return metrics_collector.get(metrics_type).get(collect_key)
+    with metrics_locks.get(metrics_type):
         if metrics_collector.get(metrics_type).get(collect_key) is None:
-            metrics_collector.get(metrics_type)[collect_key] = None
+            metrics_collector.get(metrics_type)[collect_key] = build_default_metric(metrics_type)
             return metrics_collector.get(metrics_type).get(collect_key)
-        threading.Lock.release()
 
-    @staticmethod
-    def build_default_metric(metrics_type: MetricsType):
-        if metrics_type == MetricsType.metrics_type_store:
-            return MetricValue(0)
-        if metrics_type == MetricsType.metrics_type_counter:
 
-        return
+def build_default_metric(metrics_type: MetricsType):
+    if metrics_type == MetricsType.metrics_type_timer:
+        return MetricValue(Sample(RESERVOIR_SIZE))
+    if metrics_type == MetricsType.metrics_type_counter:
+        return MetricValue(0.0, 0.0)
+    return MetricValue(0.0)
+
+
+# send httpRequest to metrics server
+def send(metric_requests: MetricMessage, url: str):
+    headers = {"Content-Type": "application/protobuf", "Accept": "application/json"}
+    req_bytes: bytes = metric_requests.SerializeToString()
+    for i in range(MAX_TRY_TIMES):
+        try:
+            response = requests.post(url=url, headers=headers, data=req_bytes,
+                                     timeout=DEFAULT_HTTP_TIMEOUT_MS / 1000)
+            if response.status_code == SUCCESS_HTTP_CODE:
+                return
+            if response.content is None:
+                raise BizException("rsp body is null")
+            raise BizException("do http request fail, url:{}， rsp:{}".format(url, response.content))
+
+        except BaseException as e:
+            if is_timeout_exception(e) and i < MAX_TRY_TIMES - 1:
+                continue
+            raise BizException(str(e))
+
+
+def is_timeout_exception(e):
+    lower_err_msg = str(e).lower()
+    if "time" in lower_err_msg and "out" in lower_err_msg:
+        return True
+    return False
--- byteplus/core/metrics/helper.py
+++ byteplus/core/metrics/helper.py
@@ -1,8 +1,65 @@
-_DELIMITER = '+'
+from constant import *
+import collector as mc
+import time
+
+
+"""
+Parameters:
+  key - metrics name
+  value - metrics value
+  tag_kvs - tag_key and tag_value list, 
+    should be formatted as ["tag_key_1:tag_value_1","tag_key_2:tag_value_2",...]
+Example:
+  counter("request.qp", 1, ["method:user", "type:upload"])
+"""
+def counter(key: str, value: float, tag_kvs: list):
+    mc.emit_counter(key, value, tag_kvs)
+
+
+"""
+Parameters:
+  key - metrics name
+  value - metrics value
+  tag_kvs - tag_key and tag_value list, 
+    should be formatted as ["tag_key_1:tag_value_1","tag_key_2:tag_value_2",...]
+Example:
+  timer("request.cost", 100, ["method:user", "type:upload"])
+"""
+def timer(key: str, value: float, tag_kvs: list):
+    mc.emit_timer(key, value, tag_kvs)
+
+
+# Latency report time cost for execution
+# tagKvs should be formatted as "key:value"
+# example: metrics.Latency("request.latency", startTime, "method:user", "type:upload")
+"""
+Parameters:
+  key - metrics name
+  begin - the unit of `begin` is milliseconds
+  tag_kvs - tag_key and tag_value list, 
+    should be formatted as ["tag_key_1:tag_value_1","tag_key_2:tag_value_2",...]
+Example:
+  latency("request.latency", start_time_ms, ["method:user", "type:upload"])
+"""
+def latency(key: str, begin: float, tag_kvs: list):
+    mc.emit_timer(key, time.time_ns() / 1e6 - begin, tag_kvs)
+
+
+"""
+Parameters:
+  key - metrics name
+  value - metrics value
+  tag_kvs - tag_key and tag_value list, 
+    should be formatted as ["tag_key_1:tag_value_1","tag_key_2:tag_value_2",...]
+Example:
+  store("goroutine.count", 400, ["ip:127.0.0.1"])
+"""
+def store(key: str, value: float, tag_kvs: list):
+    mc.emit_store(key, value, tag_kvs)
 
 
 def build_collect_key(name: str, tags: list):
-    return name + _DELIMITER + tags_to_string(tags)
+    return name + DELIMITER + tags_to_string(tags)
 
 
 def tags_to_string(tags: list):
@@ -11,10 +68,10 @@ def tags_to_string(tags: list):
 
 
 def parse_name_and_tags(src: str):
-    index = src.find(_DELIMITER)
+    index = src.find(DELIMITER)
     if index == -1:
-        return
-    return src[:index], src[index + len(_DELIMITER):]
+        return None, None, False
+    return src[:index], recover_tags(src[index + len(DELIMITER):]), True
 
 
 def recover_tags(tag_string: str) -> dict:
