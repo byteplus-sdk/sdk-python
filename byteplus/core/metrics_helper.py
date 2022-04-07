aef9cfd0118c44ab8ce34305a84f9f297070eef2
xikangfang <fangxikang@bytedance.com>
2022-04-07 18:16:10 +0800

    fix some bug

--- byteplus/core/metrics_helper.py
+++ byteplus/core/metrics_helper.py
@@ -9,16 +9,16 @@ def report_request_success(metrics_prefix: str, url: str, begin: float):
 
 def report_request_error(metrics_prefix: str, url: str, begin: float, code: int, message: str):
     url_tag = _build_url_tags(url)
-    tag_kvs = url_tag.extend(["code:" + code, "message:" + message])
-    latency(_build_latency_key(metrics_prefix), begin, tag_kvs)
-    counter(_build_counter_key(metrics_prefix), 1, tag_kvs)
+    url_tag.extend(["code:" + str(code), "message:" + message])
+    latency(_build_latency_key(metrics_prefix), begin, url_tag)
+    counter(_build_counter_key(metrics_prefix), 1, url_tag)
 
 
 def report_request_exception(metrics_prefix: str, url: str, begin: float, e: BaseException):
     url_tag = _build_url_tags(url)
-    _with_exception_tags(url_tag, e)
-    latency(_build_latency_key(metrics_prefix), begin, url_tag)
-    counter(_build_counter_key(metrics_prefix), 1, url_tag)
+    tag_kvs = _with_exception_tags(url_tag, e)
+    latency(_build_latency_key(metrics_prefix), begin, tag_kvs)
+    counter(_build_counter_key(metrics_prefix), 1, tag_kvs)
 
 
 def _with_exception_tags(tag_kvs: list, e: BaseException):
@@ -26,13 +26,14 @@ def _with_exception_tags(tag_kvs: list, e: BaseException):
     if "time" in msg and "out" in msg:
         if "connect" in msg:
             tag_kvs.append("message:connect-timeout")
-            return
+            return tag_kvs
         if "read" in msg:
             tag_kvs.append("message:read-timeout")
-            return
+            return tag_kvs
         tag_kvs.append("message:timeout")
-        return
+        return tag_kvs
     tag_kvs.append("message:other")
+    return tag_kvs
 
 
 def _build_url_tags(url: str):
